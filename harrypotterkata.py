1abf91cc9c970be47fb4955b8dab84b160eac25f
sam43125 <sam43125@ymail.com>
2022-05-12 00:55:39 +0800

    fourth green (edge cases)

--- harrypotterkata.py
+++ harrypotterkata.py
@@ -4,31 +4,49 @@ import sys
 import json
 from typing import List
 from collections import Counter
+from functools import lru_cache
+import itertools
 
 DISCOUNT_FACTORS = [1.0, 0.95, 0.9, 0.8, 0.75]
 
 def get_lowest_price(combo: List[int]) -> float:
-    if not combo:
-        return 0.0
+
+    @lru_cache(maxsize=None)
+    def __get_lowest_price(*args) -> float:
+
+        args = list(args)
+        original_args = args[:]
+        min_price = float('inf')
+        for num_different_books in range(1, sum(v > 0 for v in original_args) + 1):
+            price = 8.0 * num_different_books * DISCOUNT_FACTORS[num_different_books - 1]
+            permutations = list(itertools.permutations(num_different_books * [1] + (5 - num_different_books) * [0]))
+            for permutation in permutations:
+                args = original_args[:]
+                for book, number in enumerate(permutation):
+                    if number:
+                        if args[book] > 0:
+                            args[book] -= 1
+                        else:
+                            break
+                else:
+                    price += __get_lowest_price(*args)
+                    if price < min_price:
+                        min_price = price
+        
+        return min_price if min_price != float('inf') else 0.0
+
     counter = Counter(combo)
     occurrences = dict(counter)
+    b0 = occurrences.get(0, 0)
+    b1 = occurrences.get(1, 0)
+    b2 = occurrences.get(2, 0)
+    b3 = occurrences.get(3, 0)
+    b4 = occurrences.get(4, 0)
 
-    price = 0.0
-    while occurrences:
-
-        num_different_books = 0
-        for k in list(occurrences.keys()):
-            occurrences[k] -= 1
-            num_different_books += 1
-            if occurrences[k] <= 0:
-                del occurrences[k]
-        
-        price += 8.0 * num_different_books * DISCOUNT_FACTORS[num_different_books - 1]
-
-    return price
+    return __get_lowest_price(b0, b1, b2, b3, b4)
 
 def main(argv):
-    assert len(argv) > 2
+    assert len(argv) >= 2
     buys = json.loads(argv[1])
     print(get_lowest_price(buys))    
 
