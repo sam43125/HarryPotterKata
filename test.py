9da8ee7a16698d2b2d1678db838675bfd9325043
sam43125 <sam43125@ymail.com>
2022-05-11 23:12:52 +0800

    second red (simple discounts)

--- test.py
+++ test.py
@@ -3,10 +3,17 @@ import unittest
 from harrypotterkata import get_lowest_price
   
 class TestKata(unittest.TestCase):
-    # test function to test equality of two value
-    def test_basic(self):
+    
+    # Ref: https://codingdojo.org/kata/Potter/
+    def test_basics(self):
         self.assertEqual(0, get_lowest_price([]))
         self.assertEqual(8, get_lowest_price([1]))
+    
+    def test_simple_discounts(self):
+        self.assertEqual(8 * 2 * 0.95, get_lowest_price([0, 1]))
+        self.assertEqual(8 * 3 * 0.9, get_lowest_price([0, 2, 4]))
+        self.assertEqual(8 * 4 * 0.8, get_lowest_price([0, 1, 2, 4]))
+        self.assertEqual(8 * 5 * 0.75, get_lowest_price([0, 1, 2, 3, 4]))
   
 if __name__ == '__main__':
     unittest.main()
