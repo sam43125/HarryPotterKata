43f77c15b7396c8d57b1649bfd812a3dab57f4db
sam43125 <sam43125@ymail.com>
2022-05-11 23:41:23 +0800

    fourth red (edge cases)

--- test.py
+++ test.py
@@ -20,6 +20,15 @@ class TestKata(unittest.TestCase):
         self.assertEqual(2 * (8 * 2 * 0.95), get_lowest_price([0, 0, 1, 1]))
         self.assertEqual((8 * 4 * 0.8) + (8 * 2 * 0.95), get_lowest_price([0, 0, 1, 2, 2, 3]))
         self.assertEqual(8 + (8 * 5 * 0.75), get_lowest_price([0, 1, 1, 2, 3, 4]))
+
+    def test_edge_cases(self):
+        self.assertEqual(2 * (8 * 4 * 0.8), get_lowest_price([0, 0, 1, 1, 2, 2, 3, 4]))
+        self.assertEqual(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8), 
+            get_lowest_price([0, 0, 0, 0, 0, 
+                              1, 1, 1, 1, 1, 
+                              2, 2, 2, 2, 
+                              3, 3, 3, 3, 3, 
+                              4, 4, 4, 4]))
   
 if __name__ == '__main__':
     unittest.main()
